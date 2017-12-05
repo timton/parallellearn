39b4c523206d3849b17661ceb0c4e998d03109f7
Timofei Tonu <timofei.tonu@gmail.com>
2017-12-05 11:02:55 +0000

    added login route and template

--- application.py
+++ application.py
@@ -118,6 +118,43 @@ def log_out():
     # redirect user to welcome page
     return redirect(url_for("index"))
 
+#login route
+@app.route("/log_in", methods=["GET", "POST"])
+def log_in():
+    """Log user in."""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure email/username was submitted
+        if not request.form.get("email_or_username"):
+            return apology("must provide email or username")
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+        # query database for email or username
+        rows = db.execute("SELECT * FROM users WHERE email = :email_or_username OR username = :email_or_username",
+                          email_or_username=request.form.get("email_or_username"))
+
+        # ensure email/username exists and password is correct
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+            return apology("invalid email/username and/or password")
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
 
 ######################
 # DONE UP UNTIL HERE #
@@ -231,42 +268,6 @@ def history():
 
     # render the history template
     return render_template("history.html", transactions=transactions)
-
-@app.route("/log_in", methods=["GET", "POST"])
-def log_in():
-    """Log user in."""
-
-    # forget any user_id
-    session.clear()
-
-    # if user reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username")
-
-        # ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password")
-
-        # query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
-
-        # ensure username exists and password is correct
-        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-            return apology("invalid username and/or password")
-
-        # remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # redirect user to home page
-        return redirect(url_for("index"))
-
-    # else if user reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
 
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
