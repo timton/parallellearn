41e6274886fb9a1d7e9fe84ef9f2bc608f5e07e2
v <timofei.tonu@gmail.com>
2018-07-12 10:59:03 +0300

    fixed project card

--- application.py
+++ application.py
@@ -215,6 +215,15 @@ class Version(db.Model):
             'rating': self.rating
         }
 
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
 # configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
