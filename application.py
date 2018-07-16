0be3ac34c6247366c135541ba877990a36dbb0f2
v <timofei.tonu@gmail.com>
2018-07-17 00:50:57 +0300

    undoind, session not permanent

--- application.py
+++ application.py
@@ -226,7 +226,7 @@ if app.config["DEBUG"]:
 
 # configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
